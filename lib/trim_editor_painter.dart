a678ea9b168920dca2d55a8f9af9eaff4a3f0cca
Souvik Biswas <sbis1999@gmail.com>
2020-07-19 06:39:22 +0530

    Update Docs

--- lib/trim_editor_painter.dart
+++ lib/trim_editor_painter.dart
@@ -1,16 +1,87 @@
 import 'package:flutter/material.dart';
 
 class TrimEditorPainter extends CustomPainter {
+  /// To define the start offset
   final Offset startPos;
+
+  /// To define the end offset
   final Offset endPos;
+
+  /// To define the horizontal length of the selected video area
   final double scrubberAnimationDx;
+
+  /// For specifying a size to the holder at the
+  /// two ends of the video trimmer area, while it is `idle`.
+  /// By default it is set to `0.5`.
   final double circleSize;
+
+  /// For specifying the width of the border around
+  /// the trim area. By default it is set to `3`.
   final double borderWidth;
+
+  /// For specifying the width of the video scrubber
   final double scrubberWidth;
+
+  /// For specifying whether to show the scrubber
   final bool showScrubber;
+
+  /// For specifying a color to the border of
+  /// the trim area. By default it is set to `Colors.white`.
   final Color borderPaintColor;
+
+  /// For specifying a color to the circle.
+  /// By default it is set to `Colors.white`
   final Color circlePaintColor;
+
+  /// For specifying a color to the video
+  /// scrubber inside the trim area. By default it is set to
+  /// `Colors.white`.
   final Color scrubberPaintColor;
+
+  /// For drawing the trim editor slider
+  ///
+  /// The required parameters are [startPos], [endPos]
+  /// & [scrubberAnimationDx]
+  ///
+  /// * [startPos] to define the start offset
+  ///
+  ///
+  /// * [endPos] to define the end offset
+  ///
+  ///
+  /// * [scrubberAnimationDx] to define the horizontal length of the
+  /// selected video area
+  ///
+  ///
+  /// The optional parameters are:
+  ///
+  /// * [circleSize] for specifying a size to the holder at the
+  /// two ends of the video trimmer area, while it is `idle`.
+  /// By default it is set to `0.5`.
+  ///
+  ///
+  /// * [borderWidth] for specifying the width of the border around
+  /// the trim area. By default it is set to `3`.
+  ///
+  ///
+  /// * [scrubberWidth] for specifying the width of the video scrubber
+  ///
+  ///
+  /// * [showScrubber] for specifying whether to show the scrubber
+  ///
+  ///
+  /// * [borderPaintColor] for specifying a color to the border of
+  /// the trim area. By default it is set to `Colors.white`.
+  ///
+  ///
+  /// * [circlePaintColor] for specifying a color to the circle.
+  /// By default it is set to `Colors.white`.
+  ///
+  ///
+  /// * [scrubberPaintColor] for specifying a color to the video
+  /// scrubber inside the trim area. By default it is set to
+  /// `Colors.white`.
+  ///
   TrimEditorPainter({
     @required this.startPos,
     @required this.endPos,
