f8670c50b53efd4c0d65d912761ff9e63b9d7ceb
Souvik Biswas <sbis1999@gmail.com>
2021-09-27 08:56:13 +0530

    Refactor to fix linting issues - thumbnail viewer

--- lib/src/thumbnail_viewer.dart
+++ lib/src/thumbnail_viewer.dart
@@ -14,14 +14,15 @@ class ThumbnailViewer extends StatelessWidget {
 
   /// For showing the thumbnails generated from the video,
   /// like a frame by frame preview
-  ThumbnailViewer({
+  const ThumbnailViewer({
+    Key? key,
     required this.videoFile,
     required this.videoDuration,
     required this.thumbnailHeight,
     required this.numberOfThumbnails,
     required this.fit,
     this.quality = 75,
-  });
+  }) : super(key: key);
 
   Stream<List<Uint8List?>> generateThumbnail() async* {
     final String _videoPath = videoFile.path;
@@ -66,7 +67,7 @@ class ThumbnailViewer extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemCount: _imageBytes.length,
               itemBuilder: (context, index) {
-                return Container(
+                return SizedBox(
                   height: thumbnailHeight,
                   width: thumbnailHeight,
                   child: Image(
