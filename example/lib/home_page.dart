aa8a4cbabba3ad4eae8a410c8f746d287ed9de60
Souvik Biswas <sbis1999@gmail.com>
2021-09-27 08:55:26 +0530

    Refactor to fix linting issues - home page

--- example/lib/home_page.dart
+++ example/lib/home_page.dart
@@ -6,31 +6,31 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Video Trimmer"),
+        title: const Text("Video Trimmer"),
       ),
       body: Center(
-        child: Container(
-          child: ElevatedButton(
-            child: Text("LOAD VIDEO"),
-            onPressed: () async {
-              FilePickerResult? result = await FilePicker.platform.pickFiles(
-                type: FileType.video,
-                allowCompression: false,
+        child: ElevatedButton(
+          child: const Text("LOAD VIDEO"),
+          onPressed: () async {
+            FilePickerResult? result = await FilePicker.platform.pickFiles(
+              type: FileType.video,
+              allowCompression: false,
+            );
+            if (result != null) {
+              File file = File(result.files.single.path!);
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) {
+                  return TrimmerView(file);
+                }),
               );
-              if (result != null) {
-                File file = File(result.files.single.path!);
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                    return TrimmerView(file);
-                  }),
-                );
-              }
-            },
-          ),
+            }
+          },
         ),
       ),
     );
