a0f93c117fb023de678504c415feab95fe92e592
Matt Schleder <schlederboss@gmail.com>
2024-01-07 09:58:08 -0500

    Upgrade ffmpeg_kit_flutter to ffmpeg_kit_flutter_full

--- lib/src/trimmer.dart
+++ lib/src/trimmer.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:ffmpeg_kit_flutter_full/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_full/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_full/return_code.dart';
 import 'package:path/path.dart';
 
 import 'package:flutter/material.dart';
